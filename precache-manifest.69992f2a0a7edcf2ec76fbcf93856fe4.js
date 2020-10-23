1825e2a4ab9970bc75c5421b29b2eeee22bf0394
方誉蔓 <45160587+fangyuman@users.noreply.github.com>
2020-10-23 17:04:43 +0800

    Add files via upload

---
+++ precache-manifest.69992f2a0a7edcf2ec76fbcf93856fe4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca234242da65eaa4c739cc2f62f6405b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc0723ce646e27ebb98a",
+    "url": "./static/css/2.3786585f.chunk.css"
+  },
+  {
+    "revision": "d10d18f8dc971b55a427",
+    "url": "./static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "fc0723ce646e27ebb98a",
+    "url": "./static/js/2.7bcb7e46.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.7bcb7e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10d18f8dc971b55a427",
+    "url": "./static/js/main.979a13e5.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "f24fadf5ca4424ca89bf2d9df4d8d657",
+    "url": "./static/media/pic.f24fadf5.png"
+  }
+]);
